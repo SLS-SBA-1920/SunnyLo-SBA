31a900de31a9ee23509b243ae360d78ae16a56cf
SunnyLo <sumsum_2003@yahoo.com.hk>
2020-07-20 10:50:44 +0800

    Merge with master branch

---
+++ start - Linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+python3 main/main.py
+printf "Press Enter to continue..."
+read
---
+++ start - Windows.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+title St. Louis School ICT SBA by Lo Yung Sum
+python3 main/main.py
+pause
